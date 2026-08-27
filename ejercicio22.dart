5366a25a0076e58721b987707aedd0cbe720bca5
Carlos <melendez181205@gmail.com>
2026-08-27 17:32:10 -0500

    Limpiar formato del ejercicio 22

--- ejercicio22.dart
+++ ejercicio22.dart
@@ -9,7 +9,7 @@ void registrarAprendiz({
 void main() {
   // Uso con valor por defecto
   registrarAprendiz(nombre: 'Mariana', ficha: 2894621);
-  
+
   // Sobrescribiendo el parámetro opcional
   registrarAprendiz(nombre: 'Felipe', ficha: 2894621, centro: 'CTMA');
 }
