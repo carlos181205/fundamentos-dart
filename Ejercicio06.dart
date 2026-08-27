0ac969518a813683ae673a1e4a583f7dd727660a
Carlos <melendez181205@gmail.com>
2026-08-27 16:47:38 -0500

    Agregar Ejercicio06

---
+++ Ejercicio06.dart
@@ -0,0 +1,6 @@
+void main(){
+  //Interpolacion con cadenas
+  //Enunciado. 
+  double nota1 = 4.5, nota2 = 3.8, nota3 = 5.0;
+  print("Los promedios de los estudiantes son: $nota1 + $nota2 + $nota3/3");
+}
