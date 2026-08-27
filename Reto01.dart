43a66b62a530a7c91b2d490d0d240c0b11db989d
Carlos <melendez181205@gmail.com>
2026-08-27 16:47:39 -0500

    Agregar Reto01

---
+++ Reto01.dart
@@ -0,0 +1,11 @@
+void main(){
+  String nombre = "Carlos Melendez";
+  int ficha= 3278641;
+  double promedio = 5.0;
+  bool activo = true;
+  String? telefonoAcudiente= null;
+
+
+  print("Aprendiz $nombre con ficha $ficha tiene un promedio de $promedio y su estado es $activo. Su teléfono de acudiente es $telefonoAcudiente");
+
+}
