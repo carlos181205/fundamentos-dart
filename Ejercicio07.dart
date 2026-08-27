e8e192cac3312cc5d4ae889c4f99f73b9374b728
Carlos <melendez181205@gmail.com>
2026-08-27 16:47:38 -0500

    Agregar Ejercicio07

---
+++ Ejercicio07.dart
@@ -0,0 +1,11 @@
+void main(){
+  int numero1 = int.parse("42");
+  double nota = double.parse ("4.5");
+  double pi = 3.1416;
+  
+  print(numero1 + 8);
+  print(nota + 0.5);
+  print('El numero pi con dos decimales es: ${pi.toStringAsFixed(2)}  ');
+
+  
+}
