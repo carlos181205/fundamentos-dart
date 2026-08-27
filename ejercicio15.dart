aecead4e03d6022adeb50b12bbad3cc5b747aea6
Carlos <melendez181205@gmail.com>
2026-08-27 17:14:06 -0500

    Agregar ejercicio 15

---
+++ ejercicio15.dart
@@ -0,0 +1,15 @@
+void main() {
+  int dia = 5;
+  switch (dia) {
+    case 1:
+      print('Lunes: conceptualización y demo');
+    case 2:
+    case 3:
+    case 4:
+      print('Taller: desarrollo del mini proyecto');
+    case 5:
+      print('Viernes: checkpoint y entrega');
+    default:
+      print('Fin de semana: descanso merecido');
+  }
+}
