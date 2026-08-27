1bcc0d0796c7eb0cf07914acfedce7c9a8bbffc4
Carlos <melendez181205@gmail.com>
2026-08-27 17:25:14 -0500

    Agregar ejercicio 21

---
+++ ejercicio21.dart
@@ -0,0 +1,8 @@
+void saludarAprendiz(String nombre, String programa) {
+  print('¡Bienvenido(a), $nombre, al programa $programa!');
+}
+
+void main() {
+  saludarAprendiz('Camila', 'ADSO');
+  saludarAprendiz('Carlos', 'Redes');
+}
