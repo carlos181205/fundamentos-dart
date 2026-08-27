3f78b728cd1a3c8453ac1ba091035dfffd9296b4
Carlos <melendez181205@gmail.com>
2026-08-27 16:47:39 -0500

    Agregar Ejercicio10

---
+++ Ejercicio10.dart
@@ -0,0 +1,13 @@
+void main(){
+
+  late String mensaje;
+
+  //... otras instrucciones o logica del programa...
+
+  //Inicializacion de la variable
+  mensaje = 'Hola, Sena';
+
+  //Uso de la variable
+  print(mensaje);
+}
+
