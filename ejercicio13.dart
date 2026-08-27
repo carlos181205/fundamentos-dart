ce833f55a1aa27421afde5a8a4c4d7bbaa3a7094
Carlos <melendez181205@gmail.com>
2026-08-27 17:10:47 -0500

    Agregar ejercicio 13

---
+++ ejercicio13.dart
@@ -0,0 +1,12 @@
+void main() {
+  double nota = 4.2;
+  if (nota >= 4.6) {
+    print('Desempeño superior');
+  } else if (nota >= 4.0) {
+    print('Desempeño alto');
+  } else if (nota >= 3.0) {
+    print('Desempeño básico');
+  } else {
+    print('Desempeño bajo');
+  }
+}
