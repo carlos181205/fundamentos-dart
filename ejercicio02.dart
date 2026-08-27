8b8f1303674cba960d9ca9b7cf384c5564ab435b
Carlos <melendez181205@gmail.com>
2026-08-27 16:47:37 -0500

    Agregar ejercicio02

---
+++ ejercicio02.dart
@@ -0,0 +1,15 @@
+/*inferencia de tipos con var*/
+void main() {
+  String nombre = "Juan";
+  int edad = 25;
+  bool esEstudiante = true;
+  double promedio = 8.5;
+
+
+  print("Nombre: $nombre");
+  print("Edad: $edad");
+  print("Es estudiante: $esEstudiante");  
+  print("Promedio: $promedio");
+
+
+}
